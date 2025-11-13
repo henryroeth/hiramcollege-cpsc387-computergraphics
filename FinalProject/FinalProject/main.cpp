83542d9b89be766e709d60a0808e5b9f6e699ea5
Henry <henrykroeth@gmail.com>
2025-11-13 16:11:21 -0500

    Updates

--- FinalProject/FinalProject/main.cpp
+++ FinalProject/FinalProject/main.cpp
@@ -44,9 +44,9 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
+    #ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
 
     // glfw window creation
     // --------------------
